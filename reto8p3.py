1db15f587ad25d0fd2da8f6da291f306abc31231
DuvayOrtiz <124726079+DuvayOrtiz@users.noreply.github.com>
2023-04-17 12:33:58 -0500

    Update reto8p3.py

--- reto8p3.py
+++ reto8p3.py
@@ -2,9 +2,9 @@
 x=int(input("ingresar numero mayor igual que 2: ")) 
 def pares(x):
     c = []
-    for i in range(x+1):
+    for i in range(1,x+1):
         if i%2==0:
-            c.append(i)
+            c.insert(0,i)
     return c
 print(pares(x))
 print("listo")
